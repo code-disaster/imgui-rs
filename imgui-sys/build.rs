cf2e938faaa4fbad63e135d44aa6979120159873
Daniel Ludwig <codi@code-disaster.com>
2018-11-19 01:16:51 +0100

    ***DON'T MERGE*** use patched version to compile on x86_64-pc-windows-gnu
    removes use of stdout and printf() to avoid link errors when compiled with x86_64-pc-windows-gnu

--- imgui-sys/build.rs
+++ imgui-sys/build.rs
@@ -7,8 +7,13 @@ fn main() {
         .file("third-party/cimgui/cimgui/fontAtlas.cpp")
         .file("third-party/cimgui/cimgui/drawList.cpp")
         .file("third-party/cimgui/cimgui/listClipper.cpp")
-        .file("third-party/cimgui/imgui/imgui.cpp")
-        .file("third-party/cimgui/imgui/imgui_demo.cpp")
+        //.file("third-party/cimgui/imgui/imgui.cpp")
+        //.file("third-party/cimgui/imgui/imgui_demo.cpp")
         .file("third-party/cimgui/imgui/imgui_draw.cpp")
+        // use patched files for compiling with x86_64-pc-windows-gnu -->
+        .file("third-party.patch/imgui.cpp")
+        .file("third-party.patch/imgui_demo.cpp")
+        .flag_if_supported("-Wno-class-memaccess")
+        // <-- patch
         .compile("libcimgui.a");
 }
